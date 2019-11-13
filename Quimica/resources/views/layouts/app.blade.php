398b76c391d1f718f0cbffa28374bfc8d21749e0
Cesar Gerardo Guzman Lopez <88-8@live.com.mx>
2019-11-13 09:56:23 -0600

    ..

--- Quimica/resources/views/layouts/app.blade.php
+++ Quimica/resources/views/layouts/app.blade.php
@@ -84,6 +84,6 @@
     </div>
    </div>
    <?php  ?>
-      <script type="text/javascript" src="{{asset('js/app.js') }}?<?="ver_1.0."date("my")?>"></script>
+      <script type="text/javascript" src="{{asset('js/app.js') }}?<?="ver_1.0".date("my")?>"></script>
    </body>
 </html>
